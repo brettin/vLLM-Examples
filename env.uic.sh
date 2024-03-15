205dca0abce8d6ec76c57ec7783faa5c76d76c28
brettin <brettin@anl.gov>
2024-03-15 09:03:07 -0500

    new env

---
+++ env.uic.sh
@@ -0,0 +1,6 @@
+export HF_DATASETS_CACHE=/gpustor/brettin/.cache
+export TRANSFORMERS_CACHE=/gpustor/brettin/.cache  # depricated
+export HF_HOME=/gpustor/brettin/.cache
+
+export PATH=${PATH}:/gpustor/brettin/anaconda3/bin
+conda activate /gpustor/brettin/anaconda3/envs/vLLM
