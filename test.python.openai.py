980611778499dab2fff2662f1724da772d2a80da
brettin <brettin@anl.gov>
2024-03-23 22:04:32 -0500

    working logprobs

--- test.python.openai.py
+++ test.python.openai.py
@@ -17,8 +17,11 @@ client = OpenAI(
     base_url=openai_api_base,
 )
 
+# sampling_params = SamplingParams({"prompt_logprobs": 1, "logprobs": 1))
 chat_response = client.chat.completions.create(
     model="mistralai/Mixtral-8x7B-Instruct-v0.1",
+    # logprobs=1,
+    # top_logprobs=1,
     messages=[
         {"role": "user", "content": "A detailed description of the biochemical \
             function 5-(hydroxymethyl)furfural/furfural transporter is"},
