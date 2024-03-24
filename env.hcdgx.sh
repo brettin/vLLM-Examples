b20c964528fa144f9bc025800b7ada1301325221
Thomas Brettin <brettin@hcdgx2.cels.anl.gov>
2024-03-23 21:49:25 -0500

    new system

---
+++ env.hcdgx.sh
@@ -0,0 +1,10 @@
+export HF_DATASETS_CACHE=/raid/brettin/.cache
+export TRANSFORMERS_CACHE=/raid/brettin/.cache
+export HF_HOME=/raid/brettin/.cache
+
+export RAY_DEDUP_LOGS=0
+
+#export PATH=${PATH}:/gpustor/brettin/anaconda3/bin
+#conda activate /gpustor/brettin/anaconda3/envs/vLLM
+
+conda activate vLLM
