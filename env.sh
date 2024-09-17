55404ad67dff56cfd07e97fc12cbb36e85f4d4d3
Thomas Brettin <brettin@anl.gov>
2024-09-17 11:40:48 -0500

    changed to use brettin/.cache

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
-export HF_DATASETS_CACHE=/rbscratch/vllm/.cache
-export TRANSFORMERS_CACHE=/rbscratch/vllm/.cache  # depricated
-export HF_HOME=/rbscratch/vllm/.cache
+export HF_DATASETS_CACHE=/rbscratch/brettin/.cache
+export TRANSFORMERS_CACHE=/rbscratch/brettin/.cache  # depricated
+export HF_HOME=/rbscratch/brettin/.cache
 
 export PATH=${PATH}:/home/brettin/anaconda3/bin
 conda activate /rbscratch/brettin/conda_envs/vLLM
