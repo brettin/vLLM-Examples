f9e30f7675243c3f4a177d6c999cdd68fbaeefe9
Thomas Brettin <brettin@anl.gov>
2024-09-23 03:14:45 -0500

    new machine

---
+++ env/env.rbh101.sh
@@ -0,0 +1,6 @@
+export HF_DATASETS_CACHE=/rbscratch/brettin/.cache
+export TRANSFORMERS_CACHE=/rbscratch/brettin/.cache  # depricated
+export HF_HOME=/rbscratch/brettin/.cache
+
+export PATH=${PATH}:/home/brettin/anaconda3/bin
+conda activate vllm
