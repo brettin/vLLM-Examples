56c6a8afaf9b1949075aad086d38e0e0d7067cdb
Thomas Brettin <brettin@anl.gov>
2024-12-30 01:48:09 +0000

    add lambda13

---
+++ env/env.lambda13.sh
@@ -0,0 +1,7 @@
+export PATH=${PATH}:/homes/brettin/anaconda3/bin
+
+export HF_DATASETS_CACHE=/homes/brettin/public/.cache
+export TRANSFORMERS_CACHE=/homes/brettin/public/.cache  # depricated
+export HF_HOME=/homes/brettin/public/.cache
+
+conda activate /homes/brettin/public/anaconda3/envs/vllm-0.6.6-post1
