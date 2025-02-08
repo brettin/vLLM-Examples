5346246ca0bf5afe2ad44e9f697abfbc01c643c7
Thomas Brettin <brettin@anl.gov>
2025-02-08 15:02:09 +0000

    clean up

--- env/env.lambda13.sh
+++ env/env.lambda13.sh
@@ -1,7 +1,11 @@
 export PATH=${PATH}:/homes/brettin/anaconda3/bin
 
-export HF_DATASETS_CACHE=/homes/brettin/public/.cache
-export TRANSFORMERS_CACHE=/homes/brettin/public/.cache  # depricated
-export HF_HOME=/homes/brettin/public/.cache
+# export HF_DATASETS_CACHE=/homes/brettin/public/.cache
+# export TRANSFORMERS_CACHE=/homes/brettin/public/.cache  # depricated
+# export HF_HOME=/homes/brettin/public/.cache
+
+export HF_DATASETS_CACHE=/homes/brettin/.cache
+export TRANSFORMERS_CACHE=/homes/brettin/.cache  # depricated
+export HF_HOME=/homes/brettin/.cache
 
 conda activate /homes/brettin/public/anaconda3/envs/vllm-0.6.6-post1
