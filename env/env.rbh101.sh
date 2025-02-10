4c0ed9c6dc5135ffdddba07bd5886e6bc2c23588
Thomas Brettin <brettin@anl.gov>
2025-02-10 12:23:52 -0600

    new python env

--- env/env.rbh101.sh
+++ env/env.rbh101.sh
@@ -3,4 +3,9 @@ export TRANSFORMERS_CACHE=/rbscratch/brettin/.cache  # depricated
 export HF_HOME=/rbscratch/brettin/.cache
 
 export PATH=${PATH}:/home/brettin/anaconda3/bin
-conda activate vllm
+conda activate vllm-0.6.6-post1
+
+# export OMP_NUM_THREADS=192
+# When setting:
+# --kv-cache-dtype fp8
+# export VLLM_ATTENTION_BACKEND=FLASHINFER
